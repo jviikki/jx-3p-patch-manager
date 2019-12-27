8ac60995872964379c71cfec4f075cf0a8d6e3bd
Jarkko Viikki <jarkko.viikki@gmail.com>
2019-12-27 22:27:19 +0200

    Extracted samples from bit_0.wav and bit_1.wav into C constants

---
+++ encoder/audio.h
@@ -0,0 +1,7 @@
+#ifndef AUDIO_H
+#define AUDIO_H
+
+extern const short AUDIO_BIT_ZERO[];
+extern const short AUDIO_BIT_ONE[];
+
+#endif // AUDIO_H
--- encoder/encoder.c
+++ encoder/encoder.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "sndfile.h"
 
+#define BUFFER_SIZE 1024
+
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         printf("USAGE: %s <audio_file>\n", argv[0]);
@@ -19,6 +21,14 @@ int main(int argc, char *argv[]) {
 
     printf("frames: %lld, samplerate: %d, channels: %d, format: %d, sections: %d, seekable: %d\n",
         info.frames, info.samplerate, info.channels, info.format, info.sections, info.seekable);
+
+    short samples[BUFFER_SIZE];
+    sf_count_t items_read = 0;
+    while ((items_read = sf_read_short(file, &samples[0], BUFFER_SIZE))) {
+        for(uint i = 0; i < items_read; i++) {
+            printf("%d\n", samples[i]);
+        }
+    }
 
     int error = sf_close(file);
     if (error) {
